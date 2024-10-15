ee1e4b1a5a40045ae97c0be849fad37ad1bcc409
Arif Ali Mirani <arifalimiranixwave@gmail.com>
2024-10-15 12:56:53 +0500

    9- local storegegatItems complete

--- script/login.js
+++ script/login.js
@@ -11,8 +11,32 @@ submitDetails.addEventListener ('submit', function(e){
 
     if(userEmail && userpassword){
         localStorage.setItem('Email',userEmail)
-        localStorage.setItem('password',userpassword)
+        localStorage.setItem('password',userpassword) 
     }
 
 });
+
+let loginForm = document.getElementById('loginForm')
+
+loginForm.addEventListener ('submit' , function (e){
+    e.preventDefault()
+
+    let localMail = localStorage.getItem('Email')
+    let localpassword = localStorage.getItem('password')
+
+
+    let loginEmail = document.getElementById('loginEmail').value
+    let loginPassword = document.getElementById('loginPassword').value
+  
+
+   if( loginEmail === localMail && loginPassword === localpassword){
+    alert('Login Successful')
+    window.location.href = '../index.html'
+   } else {
+    alert('Some thhing wrong')
+
+
+}
+    
+})
 
